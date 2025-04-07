5a373b96ab3f37b1937eb618ce2cafb0c9895734
Braden <bradenleach111@gmail.com>
2025-04-07 08:53:24 -0400

    Initial commit

---
+++ NMC_C#/Cereal.cs
@@ -0,0 +1,26 @@
+namespace Cereals
+{
+    public class Snap
+    {
+        public static void talk()
+        {
+            Console.Write("Snap ");
+        }
+    }
+
+    public class Crackle
+    {
+        public static void talk()
+        {
+            Console.Write("Crackle ");
+        }
+    }
+
+    public class Pop
+    {
+        public static void talk()
+        {
+            Console.Write("Pop ");
+        }
+    }
+}
---
+++ NMC_C#/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using Cereals;
+
+namespace Ch9Ex6RiceKrispies
+{
+    class Breakfast
+    {
+        static void Main(string[] args)
+        {
+            Snap.talk();
+            Crackle.talk();
+            Pop.talk();
+            Console.Write(" Rice Krispies");
+            Console.WriteLine();
+        }
+    }
+}
